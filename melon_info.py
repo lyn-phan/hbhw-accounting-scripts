0b60b7192c07f126116c61b798696d972a3b1800
LYNDA PHAN <lynda@lyndas-mbp.lan>
2020-10-06 00:23:55 -0700

    Added new attributes

--- melon_info.py
+++ melon_info.py
@@ -1,18 +1,62 @@
 """Print out all the melons in our inventory."""
 
+# from melons import melons
 
-from melons import melon_names, melon_seedlessness, melon_prices
+melon_data = {
+    'Honeydew': {
+    'price': 0.99,
+    'seedlessness': True
+    }, 
+    'Crenshaw': {
+    'price': 2.00,
+    'seedlessness': False
+    },
+    'Crane': {
+    'price': 2.50,
+    'seedlessness': False
+    },
+    'Casaba': {
+    'price': 2.50,
+    'seedlessness': False
+    },
+    'Cantaloupe': {
+    'price': .99,
+    'seedlessness': False
+    },
+}
 
+def add_melon_attributes(melons):
+    for key, value in melon_data.items():
+        if key in melon_data:
+            melon_data[key] = [melon_data[key], 'flesh_color:']
+            melon_data[key] = [melon_data[key], 'weight:']
+            melon_data[key] = [melon_data[key], 'rind_color:']
+            return melons
 
-def print_melon(name, seedless, price):
+print_melon(melon_data)
+
+def print_melon(melons):
     """Print each melon with corresponding attribute information."""
+    for melon_name, attributes in melons.items():
+        print(melon_name)
+        for new_attribute, value in attributes.items():
+            print(f'{new_attribute}: {value}')
 
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
-
-    print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+print(add_melon_attributes(melon_data))
 
 
-for i in melon_names:
-    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+
+
+    # have_or_have_not = 'have'
+    # if seedless:
+    #     have_or_have_not = 'do not have'
+
+    # print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+
+
+# for i in melon_names:
+#     print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+
+
+
+
--- melons.py
+++ melons.py
@@ -1,23 +1,64 @@
-melon_names = {
-    1: 'Honeydew',
-    2: 'Crenshaw',
-    3: 'Crane',
-    4: 'Casaba',
-    5: 'Cantaloupe',
+melon_data = {
+    'Honeydew': {
+    'price': 0.99,
+    'seedlessness': True
+    }, 
+    'Crenshaw': {
+    'price': 2.00,
+    'seedlessness': False
+    },
+    'Crane': {
+    'price': 2.50,
+    'seedlessness': False
+    },
+    'Casaba': {
+    'price': 2.50,
+    'seedlessness': False
+    },
+    'Cantaloupe': {
+    'price': .99,
+    'seedlessness': False
+    },
 }
 
-melon_prices = {
-    1: 0.99,
-    2: 2.00,
-    3: 2.50,
-    4: 2.50,
-    5: 0.99,
-}
+def add_melon_attributes(melons):
+    for key, value in melon_data.items():
+        if key in melon_data:
+            melon_data[key] = [melon_data[key], 'flesh_color:']
+            melon_data[key] = [melon_data[key], 'weight:']
+            melon_data[key] = [melon_data[key], 'rind_color:']
+            return melons
 
-melon_seedlessness = {
-    1: True,
-    2: False,
-    3: False,
-    4: False,
-    5: False,
-}
+print(add_melon_attributes(melon_data))
+
+
+
+
+# melon_names = {
+#     1: 'Honeydew': 
+#     2: 'Crenshaw',
+#     3: 'Crane',
+#     4: 'Casaba',
+#     5: 'Cantaloupe',
+# }
+
+# melon_prices = {
+#     1: 0.99,
+#     2: 2.00,
+#     3: 2.50,
+#     4: 2.50,
+#     5: 0.99,
+# }
+
+# melon_seedlessness = {
+#     1: True,
+#     2: False,
+#     3: False,
+#     4: False,
+#     5: False,
+
+
+
+
+
+
